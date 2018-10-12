ba6296499cf7af8a2a65296f2c540d5804f29a6d
nekr0s <alexanderrusev@abv.bg>
2018-10-12 20:19:02 +0300

    Initial commit - Login Activity

---
+++ app/src/main/java/com/example/nekr0s/get_my_driver_card/LoginActivity.java
@@ -0,0 +1,127 @@
+package com.example.nekr0s.get_my_driver_card;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import studios.codelight.smartloginlibrary.LoginType;
+import studios.codelight.smartloginlibrary.SmartLogin;
+import studios.codelight.smartloginlibrary.SmartLoginCallbacks;
+import studios.codelight.smartloginlibrary.SmartLoginConfig;
+import studios.codelight.smartloginlibrary.SmartLoginFactory;
+import studios.codelight.smartloginlibrary.UserSessionManager;
+import studios.codelight.smartloginlibrary.users.SmartUser;
+import studios.codelight.smartloginlibrary.util.SmartLoginException;
+
+public class LoginActivity extends AppCompatActivity implements SmartLoginCallbacks {
+
+    @BindView(R.id.button_login_facebook)
+    Button mFacebookLoginButton;
+
+    @BindView(R.id.button_login_google)
+    Button mGoogleLoginButton;
+
+    @BindView(R.id.button_login_custom)
+    Button mCustomLoginButton;
+
+    @BindView(R.id.text_no_account)
+    TextView mNoAccount;
+
+    @BindView(R.id.email_edittext)
+    EditText mEmailEditText;
+
+    @BindView(R.id.password_edittext)
+    EditText mPasswordEditText;
+
+    SmartUser mCurrentUser;
+    SmartLoginConfig mConfig;
+    SmartLogin mSmartLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        mConfig = new SmartLoginConfig(this, this);
+        mConfig.setFacebookAppId(getString(R.string.facebook_app_id));
+        mConfig.setFacebookPermissions(null);
+        mConfig.setGoogleApiClient(null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCurrentUser = UserSessionManager.getCurrentUser(this);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (mSmartLogin != null)
+            mSmartLogin.onActivityResult(requestCode, resultCode, data, mConfig);
+    }
+
+    @Override
+    public void onLoginSuccess(SmartUser user) {
+        Toast.makeText(this, user.toString(), Toast.LENGTH_SHORT).show();
+        navToHome();
+    }
+
+    @Override
+    public void onLoginFailure(SmartLoginException e) {
+        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public SmartUser doCustomLogin() {
+        SmartUser user = new SmartUser();
+        user.setEmail(mEmailEditText.getText().toString());
+        return user;
+    }
+
+    @Override
+    public SmartUser doCustomSignup() {
+        SmartUser user = new SmartUser();
+        user.setEmail(mEmailEditText.getText().toString());
+        return user;
+    }
+
+    private void navToHome() {
+        Toast.makeText(this, "Navigating to Home", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.button_login_facebook)
+    void facebookLoginClicked(){
+        mSmartLogin = SmartLoginFactory.build(LoginType.Facebook);
+        mSmartLogin.login(mConfig);
+    }
+
+    @OnClick(R.id.button_login_google)
+    void googleLoginClicked(){
+        mSmartLogin = SmartLoginFactory.build(LoginType.Google);
+        mSmartLogin.login(mConfig);
+    }
+
+    @OnClick(R.id.button_login_custom)
+    void customLoginClicked(){
+        mSmartLogin = SmartLoginFactory.build(LoginType.CustomLogin);
+        mSmartLogin.login(mConfig);
+    }
+
+    @OnClick(R.id.text_no_account)
+    void customCreateAccountClicked(){
+        mSmartLogin = SmartLoginFactory.build(LoginType.CustomSignup);
+        mSmartLogin.signup(mConfig);
+    }
+}
