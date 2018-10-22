659e2a99e262a85c70297eacf8a25c7eb9cc35cf
Wishiper <wishiper@gmail.com>
2018-10-23 01:14:16 +0300

    NewCardFragment UI design + features
    Signed-off-by: Wishiper <wishiper@gmail.com>

--- app/src/main/java/com/example/nekr0s/get_my_driver_card/views/create/fragments/NewCardFragment.java
+++ app/src/main/java/com/example/nekr0s/get_my_driver_card/views/create/fragments/NewCardFragment.java
@@ -2,12 +2,19 @@ package com.example.nekr0s.get_my_driver_card.views.create.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.nekr0s.get_my_driver_card.R;
+
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NewCardFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,6 +23,24 @@ public class NewCardFragment extends Fragment {
     public NewCardFragment() {
         // Required empty public constructor
     }
+
+    @BindView(R.id.first_name)
+    TextInputLayout mTIL_firstName;
+
+    @BindView(R.id.last_name)
+    TextInputLayout mTIL_lastName;
+
+    @BindView(R.id.personal_number)
+    TextInputLayout mTIL_personalNumber;
+
+    @BindView(R.id.address_layout)
+    TextInputLayout mTIL_address;
+
+    @BindView(R.id.phone_number)
+    TextInputLayout mTIL_phoneNumber;
+
+    @BindView(R.id.date_of_birth)
+    TextInputLayout mTIL_dateOfBirth;
 
 
     public static NewCardFragment newInstance() {
@@ -26,12 +51,15 @@ public class NewCardFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_new_card_info, container, false);
+        ButterKnife.bind(this, view);
+
         return view;
     }
 
@@ -45,7 +73,7 @@ public class NewCardFragment extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        getActivity().finish();
+        Objects.requireNonNull(getActivity()).finish();
     }
 
 }
