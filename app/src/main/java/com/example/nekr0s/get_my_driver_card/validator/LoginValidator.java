176ea092d455310ee20cfc361d96934e6fc0c239
Wishiper <wishiper@gmail.com>
2018-10-22 20:54:58 +0300

    example validation for login/register/fields
    Signed-off-by: Wishiper <wishiper@gmail.com>

--- app/src/main/java/com/example/nekr0s/get_my_driver_card/validator/LoginValidator.java
+++ app/src/main/java/com/example/nekr0s/get_my_driver_card/validator/LoginValidator.java
@@ -1,7 +1,7 @@
 package com.example.nekr0s.get_my_driver_card.validator;
 
-import com.example.nekr0s.get_my_driver_card.utils.Constants;
 import com.example.nekr0s.get_my_driver_card.models.User;
+import com.example.nekr0s.get_my_driver_card.utils.Constants;
 import com.example.nekr0s.get_my_driver_card.validator.base.Validator;
 
 public class LoginValidator implements Validator<User> {
@@ -22,5 +22,20 @@ public class LoginValidator implements Validator<User> {
         return user.getPassword().length() >= Constants.PASSWORD_MIN_LENGTH &&
                 user.getPassword().length() <= Constants.PASSWORD_MAX_LENGTH;
     }
+
+//    private boolean validateEmail(TextInputLayout textInputEmail) {
+//        String emailInput = textInputEmail.getEditText().getText().toString().trim();
+//
+//        if (emailInput.isEmpty()) {
+//            textInputEmail.setError("Field can't be empty");
+//            return false;
+//        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+//            textInputEmail.setError("Please enter a valid email address");
+//            return false;
+//        } else {
+//            textInputEmail.setError(null);
+//            return true;
+//        }
+//    }
 
 }
