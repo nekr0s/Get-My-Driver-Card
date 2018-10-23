6ee492fe5ddfc6fdef97eff28af2d516e8ec9242
nekr0s <alexanderrusev@abv.bg>
2018-10-23 21:59:50 +0300

    Replace fragment looks good
    Needs validation logic

--- app/src/main/java/com/example/nekr0s/get_my_driver_card/views/create/fragments/ReplaceFragment.java
+++ app/src/main/java/com/example/nekr0s/get_my_driver_card/views/create/fragments/ReplaceFragment.java
@@ -2,6 +2,8 @@ package com.example.nekr0s.get_my_driver_card.views.create.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.button.MaterialButton;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +25,15 @@ public class ReplaceFragment extends Fragment {
 
     @BindView(R.id.reason_list_view)
     ListView listView;
+
+    @BindView(R.id.replace_date)
+    TextInputLayout mTIL_Date;
+
+    @BindView(R.id.replace_place)
+    TextInputLayout mTIL_Place;
+
+    @BindView(R.id.replace_reason_button)
+    MaterialButton mNextButton;
 
     private int preselectedIndex = -1;
 
@@ -65,7 +76,10 @@ public class ReplaceFragment extends Fragment {
 
     private void checkBoxLogic(Reason reason, ReasonsAdapter adapter, int position) {
         if (reason.isSelected()) reason.setSelected(false);
-        else reason.setSelected(true);
+        else {
+            reason.setSelected(true);
+            displayForms(reason.getReasonName());
+        }
 
         adapter.update(position, reason);
 
@@ -77,5 +91,19 @@ public class ReplaceFragment extends Fragment {
         }
 
         preselectedIndex = position;
+    }
+
+    private boolean isLostOrStolen(String reasonName) {
+        return reasonName.contains("lost") || reasonName.contains("stolen");
+    }
+
+    private void displayForms(String reasonName) {
+        if (isLostOrStolen(reasonName)) {
+            mTIL_Date.setVisibility(View.VISIBLE);
+            mTIL_Place.setVisibility(View.VISIBLE);
+        } else {
+            mTIL_Date.setVisibility(View.GONE);
+            mTIL_Place.setVisibility(View.GONE);
+        }
     }
 }
