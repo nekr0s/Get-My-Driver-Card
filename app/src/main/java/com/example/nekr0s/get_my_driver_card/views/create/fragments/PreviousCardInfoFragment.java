5fb2272d6d2f2915fe1f2a241ed1f159b8e751f0
Wishiper <wishiper@gmail.com>
2018-10-23 18:24:50 +0300

    Fragment PreviousCardInfo added + XML changes
    Signed-off-by: Wishiper <wishiper@gmail.com>

--- app/src/main/java/com/example/nekr0s/get_my_driver_card/views/create/fragments/PreviousCardInfoFragment.java
+++ app/src/main/java/com/example/nekr0s/get_my_driver_card/views/create/fragments/PreviousCardInfoFragment.java
@@ -2,50 +2,53 @@ package com.example.nekr0s.get_my_driver_card.views.create.fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.nekr0s.get_my_driver_card.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RenewFragment extends Fragment {
+public class PreviousCardInfoFragment extends Fragment {
 
 
-    public RenewFragment() {
+    public PreviousCardInfoFragment() {
         // Required empty public constructor
     }
 
-    public static Fragment newInstance() {
-        return new RenewFragment();
-    }
+    @BindView(R.id.previous_eu_country_of_issuing)
+    TextInputLayout mTIL_previous_eu_country_of_issuing;
 
-    @BindView(R.id.some_button)
-    Button mSomebutton;
+    @BindView(R.id.issuing_authority)
+    TextInputLayout mTIL_issuing_authority;
+
+    @BindView(R.id.previous_tachograph_card_number)
+    TextInputLayout mTIL_previous_tachograph_card_number;
+
+    @BindView(R.id.date_of_expiry)
+    TextInputLayout mTIL_date_of_expiry;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_renew, container, false);
+        View view = inflater.inflate(R.layout.previous_card_info, container, false);
         ButterKnife.bind(this, view);
 
         return view;
     }
-
-    @OnClick(R.id.some_button)
-
 
     @Override
     public void onDetach() {
         super.onDetach();
         getActivity().finish();
     }
+
+
 }
